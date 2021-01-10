6318bc2ce70a4bbef1427228d2a88eebc99cb58f
Yuta Koreeda <yuta.koreeda@hal.hitachi.com>
2021-01-09 18:21:56 -0800

    Implement paralell feature importance calculation

--- feature_importance.py
+++ feature_importance.py
@@ -2,6 +2,8 @@ import json
 import os
 
 import click
+from joblib import Parallel, delayed
+
 
 from pdf_struct import transition_labels, transition_predictor
 from pdf_struct.pdf import load_pdfs
@@ -9,11 +11,26 @@ from pdf_struct.structure_evaluation import evaluate_structure, evaluate_labels
 from pdf_struct.text import load_texts
 
 
+def single_run(documents, feature_indices, i):
+    documents_pred = transition_predictor.k_fold_train_predict(
+        documents, used_features=feature_indices)
+    structure_metrics = evaluate_structure(documents, documents_pred)
+    label_metrics = evaluate_labels(documents, documents_pred,
+                                    confusion_matrix=False)
+    return {
+        'target_feature': i,
+        'used_features': feature_indices,
+        'structure_metrics': structure_metrics,
+        'label_metrics': label_metrics
+    }
+
+
 @click.command()
 @click.argument('file-type', type=click.Choice(('txt', 'pdf')))
 @click.argument('search-method', type=click.Choice(('incr-important', 'decr-important', 'decr-unimportant')))
 @click.argument('n-rounds', type=int)
-def main(file_type: str, search_method: str, n_rounds: int):
+@click.option('--n-jobs', type=int, default=1)
+def main(file_type: str, search_method: str, n_rounds: int, n_jobs: int):
     anno_dir = os.path.join('data', f'anno_{file_type}')
     print(f'Loading annotations from {anno_dir}')
     annos = transition_labels.load_annos(anno_dir)
@@ -24,7 +41,6 @@ def main(file_type: str, search_method: str, n_rounds: int):
     else:
         documents = load_texts(os.path.join('data', 'raw'), annos)
 
-    import pdb; pdb.set_trace()
     n_features = len(documents[0].feats[0])
     if n_rounds <= 0:
         n_rounds = n_features
@@ -32,38 +48,34 @@ def main(file_type: str, search_method: str, n_rounds: int):
     results = []
     cur_feature_indices = set()
     for round in range(n_rounds):
-        metrics = []
-        results_round = []
-        for i in all_feature_indices - cur_feature_indices:
-            if search_method == 'incr-important':
-                feature_indices = sorted(cur_feature_indices | {i})
-            else:
-                feature_indices = sorted(
-                    all_feature_indices - (cur_feature_indices | {i}))
-            documents_pred = transition_predictor.k_fold_train_predict(documents, used_features=feature_indices)
-            structure_metrics = evaluate_structure(documents, documents_pred)
-            label_metrics = evaluate_labels(documents, documents_pred, confusion_matrix=False)
-            results_round.append({
-                'target_feature': i,
-                'used_features': feature_indices,
-                'structure_metrics': structure_metrics,
-                'label_metrics': label_metrics
-            })
-            metrics.append((i, label_metrics['accuracy']['micro']))
-        if search_method == 'incr-important' or search_method == 'decr-unimportant':
-            chosen_ind, acc = max(metrics, key=lambda i_m: i_m[1])
+        if search_method == 'incr-important':
+            results_round = Parallel(n_jobs=n_jobs)(
+                delayed(single_run)(documents, sorted(cur_feature_indices | {i}), i)
+                for i in all_feature_indices - cur_feature_indices)
         else:
-            chosen_ind, acc = min(metrics, key=lambda i_m: i_m[1])
+            results_round = Parallel(n_jobs=n_jobs)(
+                delayed(single_run)(documents, all_feature_indices - (cur_feature_indices | {i}), i)
+                for i in all_feature_indices - cur_feature_indices)
 
-        print(f'Chosen feature #{chosen_ind} with accuracy {acc}')
-        cur_feature_indices.add(chosen_ind)
+        if search_method == 'incr-important' or search_method == 'decr-unimportant':
+            result_chosen = max(
+                results_round,
+                key=lambda r: r['label_metrics']['accuracy']['micro'])
+        else:
+            result_chosen = min(
+                results_round,
+                key=lambda r: r['label_metrics']['accuracy']['micro'])
+
+        print(f'Chosen feature #{result_chosen["target_feature"]} with accuracy '
+              f'{result_chosen["label_metrics"]["accuracy"]["micro"]}')
+        cur_feature_indices.add(result_chosen["target_feature"])
         results.append({
             'round': round,
-            'chosen_feature': chosen_ind,
+            'chosen_feature': result_chosen["target_feature"],
             'results': results_round
         })
     with open(os.path.join('data', f'results_importance_{file_type}_{search_method}.json'), 'w') as fout:
-        json.dump(results, fout)
+        json.dump(results, fout, indent=2)
 
 
 if __name__ == '__main__':
