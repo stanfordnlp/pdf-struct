a105f750b7e38cc010c5480963832d0ce9ae8247
Yuta Koreeda <yuta.koreeda@hal.hitachi.com>
2021-07-19 18:50:27 -0700

    Made SectionNumber more abstract

--- pdf_struct/listing.py
+++ pdf_struct/listing.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import List, Set, Tuple, Optional, Union, NamedTuple
+from inspect import signature
+from typing import List, Set, Optional, Union, Pattern, Type, Callable
 
 import regex as re
 
@@ -64,70 +65,119 @@ def alphabet_to_int(expr: str):
     return ord(expr) - ord('a') + 1
 
 
-class SectionNumberType(Enum):
-    NUM = 1
-    ALPH_UPPER = 2
-    ALPH_LOWER = 3
-    ROMAN_UPPER = 4
-    ROMAN_LOWER = 5
-    NUM_MULTILEVEL = 6
-    BULLET_POINT = 7
+class _SectionPatternRegisterer(type):
+
+    def __init__(cls, name, bases, attrs):
+        cls._patterns = []
+        for key, val in attrs.items():
+            properties = getattr(val, '_prop', None)
+            if properties is not None:
+                cls._patterns.append({'func': val, 'name': properties['name']})
 
 
-class SectionNumber(NamedTuple):
-    number: Optional[int]
-    section_number_type: Tuple[SectionNumberType, Union[str, int]]
+def section_pattern(name: Optional[str] = None):
+    if not (name is None or isinstance(name, str)):
+        raise ValueError(f'name must be str but {type(name)} was given.')
+    def _body(func: Callable[[str], Optional[Union[str, int]]]):
+        nonlocal name
+        n_params = len(signature(func).parameters)
+        if n_params != 1:
+            raise TypeError(
+                '@section_pattern() must receive a function with 1 argument.')
+        if name is None:
+            name = func.__name__
 
-    def is_next_of(self, other: 'SectionNumber') -> bool:
+        func._prop = {'name': name}
+        return func
+
+    return _body
+
+
+def register_section_pattern(name: str, patterns: List[Pattern[str]], convert_fun: Callable[[str], int]):
+
+    def _body(cls: Type[BaseSectionNumber]):
+        nonlocal patterns, name, convert_fun
+        if not issubclass(cls, BaseSectionNumber):
+            raise TypeError(
+                '@register_section_pattern may only be used on a class inheriting BaseSectionNumber')
+        for pattern in patterns:
+
+            def _extract_section_number_factory(pattern):
+                # use factory pattern to embed pattern into _extract_section_number
+                def _extract_section_number(text: str):
+                    m = pattern.match(text)
+                    return None if m is None else convert_fun(m.group('num'))
+                return _extract_section_number
+
+            extract_section_number = _extract_section_number_factory(pattern)
+            extract_section_number._prop = {'name': name}
+            cls._patterns.append(
+                {'func': extract_section_number,
+                 'name': name,
+                 'pattern': pattern})
+        return cls
+
+    return _body
+
+
+class BaseSectionNumber(object, metaclass=_SectionPatternRegisterer):
+
+    def __init__(self, section_number_type: str, number: Union[int, str]):
+        if not isinstance(section_number_type, str):
+            raise TypeError(f'section_number_type must be str but {type(section_number_type)} was given.')
+        if not isinstance(number, (int, str)):
+            raise TypeError(f'number must be int or str but {type(number)} was given.')
+        self.section_number_type: str = section_number_type
+        self.number: Optional[int] = number
+
+    def __eq__(self, other):
+        return (type(self) == type(other) and
+                self.number == other.number and
+                self.section_number_type == other.section_number_type)
+
+    @classmethod
+    def extract_section_number(cls, text: str) -> List['BaseSectionNumber']:
+        text = text.strip()
+        candidates = []
+        for pattern in cls._patterns:
+            section_number = pattern['func'](text)
+            if section_number is not None:
+                candidates.append(cls(pattern['name'], section_number))
+        return candidates
+
+    def is_next_of(self, other: 'BaseSectionNumber') -> bool:
         return other.next() == self
 
-    def next(self) -> 'SectionNumber':
-        if self.number is None:
+    def next(self) -> 'BaseSectionNumber':
+        if isinstance(self.number, str):
             # bullet points
             return self
         else:
             # Numbered list
-            return SectionNumber(self.number + 1, self.section_number_type)
+            return type(self)(self.section_number_type, self.number + 1)
 
     @staticmethod
-    def is_any_next_of(following_section_numbers: Set['SectionNumber'],
-                       section_numbers: Set['SectionNumber']) -> bool:
+    def is_any_next_of(following_section_numbers: Set['BaseSectionNumber'],
+                       section_numbers: Set['BaseSectionNumber']) -> bool:
         for s1 in section_numbers:
             for s2 in following_section_numbers:
                 if s2.is_next_of(s1):
                     return True
         return False
 
-    @staticmethod
-    def extract_section_number(text: str) -> Set['SectionNumber']:
-        text = text.strip()
-        candidates = set()
 
-        def _extract_section_number(
-                patterns: list, num_type: SectionNumberType, conv_fun):
-            for i, pat in enumerate(patterns):
-                m = pat.match(text)
-                if m is not None:
-                    s = SectionNumber(conv_fun(m.group('num')), (num_type, i))
-                    candidates.add(s)
+@register_section_pattern('arabic', _PATS_NUM, int)
+@register_section_pattern('roman_upper', _PATS_ROMAN_UPPER, roman_to_int)
+@register_section_pattern('roman_lower', _PATS_ROMAN_LOWER, roman_to_int)
+@register_section_pattern('alph_upper', _PATS_ALPH_UPPER, alphabet_to_int)
+@register_section_pattern('alph_lower', _PATS_ALPH_LOWER, alphabet_to_int)
+@register_section_pattern('arabic_multilevel', _PATS_NUM_MULTILEVEL, int)
+class SectionNumber(BaseSectionNumber):
 
-        _extract_section_number(_PATS_NUM, SectionNumberType.NUM, int)
-        _extract_section_number(
-            _PATS_ROMAN_UPPER, SectionNumberType.ROMAN_UPPER, roman_to_int)
-        _extract_section_number(
-            _PATS_ROMAN_LOWER, SectionNumberType.ROMAN_LOWER, roman_to_int)
-        _extract_section_number(
-            _PATS_ALPH_UPPER, SectionNumberType.ALPH_UPPER, alphabet_to_int)
-        _extract_section_number(
-            _PATS_ALPH_LOWER, SectionNumberType.ALPH_LOWER, alphabet_to_int)
-        _extract_section_number(
-            _PATS_NUM_MULTILEVEL, SectionNumberType.NUM_MULTILEVEL, int)
-
+    @section_pattern()
+    def bullet_point(text: str):
         m = _PAT_BULLET_POINTS.match(text)
-        if m is not None:
-            candidates.add(
-                SectionNumber(None, (SectionNumberType.BULLET_POINT, m.group(0))))
-        return candidates
+        return None if m is None else m.group(0)
 
 
 class NumberedListState(Enum):
@@ -140,9 +190,9 @@ class NumberedListState(Enum):
 
 class MultiLevelNumberedList(object):
     def __init__(self):
-        self._numbered_list: List[SectionNumber] = []
+        self._numbered_list: List[BaseSectionNumber] = []
 
-    def try_append(self, candidates: Set[SectionNumber]) -> NumberedListState:
+    def try_append(self, candidates: Set[BaseSectionNumber]) -> NumberedListState:
         if len(candidates) == 0:
             return NumberedListState.NO_NUM
         for i, section_number_pre in enumerate(self._numbered_list[::-1]):
@@ -156,7 +206,7 @@ class MultiLevelNumberedList(object):
                         return NumberedListState.UP
         # No valid continuation found... check if it is a new level
         for section_number in candidates:
-            if section_number.number is None or section_number.number <= 1:
+            if isinstance(section_number.number, str) or section_number.number <= 1:
                 self._numbered_list.append(section_number)
                 return NumberedListState.DOWN
         return NumberedListState.UNKNOWN
