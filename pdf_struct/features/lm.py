[bugfix] Wrong LM calculation

pdf_struct/features/lm.py
@@ -38,16 +38,9 @@ def _init_lm(lang):
     model_lang = lang
 
 
-def _get_masked_loss(token_ids: List[int], next: Optional[List[int]]=None,
-                     prev: Optional[List[int]]=None):
-    if prev is None == next is None:
-        raise ValueError('One and only one of prev and next should be specified.')
-    if next is not None:
-        labels = token_ids + [-100] * len(next)
-        token_ids = token_ids + next
-    else:
-        labels = [-100] * len(prev) + token_ids
-        token_ids = prev + token_ids
+def _get_masked_loss(token_ids: List[int], context: List[int]):
+    labels = [-100] * len(context) + token_ids
+    token_ids = context + token_ids
     token_ids = torch.tensor(token_ids)[:model.config.n_positions]
     labels = torch.tensor(labels)[:model.config.n_positions]
     if torch.cuda.is_available():
@@ -66,9 +59,9 @@ def compare_losses(lang: str, cand1, cand2, prev=None, next=None):
     ids_cand1 = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(cand1))
     ids_cand2 = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(cand2))
     if prev is not None:
-        loss1 = _get_masked_loss(ids_cand1, prev=ids_context)
-        loss2 = _get_masked_loss(ids_cand2, prev=ids_context)
+        loss1 = _get_masked_loss(ids_cand1, ids_context)
+        loss2 = _get_masked_loss(ids_cand2, ids_context)
     else:
-        loss1 = _get_masked_loss(ids_cand1, next=ids_context)
-        loss2 = _get_masked_loss(ids_cand2, next=ids_context)
+        loss1 = _get_masked_loss(ids_context, ids_cand1)
+        loss2 = _get_masked_loss(ids_context, ids_cand2)
     return loss1 - loss2