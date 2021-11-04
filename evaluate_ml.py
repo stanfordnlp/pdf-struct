43a96bc620a5e97ffbd07da957256e1199833ce8
Yuta Koreeda <yuta.koreeda@hal.hitachi.com>
2021-11-03 23:40:57 -0700

    Moved actual implementation of evaluation REASON: Users can utilize the evaluation in their code

--- evaluate_ml.py
+++ evaluate_ml.py
@@ -1,18 +1,11 @@
 import json
-import os
-from collections import Counter
 
 import click
-import numpy as np
-import tqdm
-from sklearn.metrics import accuracy_score
 
-from pdf_struct import loader
-from pdf_struct.core import predictor, transition_labels
-from pdf_struct.core.structure_evaluation import evaluate_structure, \
-    evaluate_labels
 from pdf_struct import feature_extractor
-
+from pdf_struct import loader
+from pdf_struct.core import transition_labels
+from pdf_struct.core.evaluation import evaluate
 
 feature_extractor_dict = {
     'HOCRFeatureExtractor': feature_extractor.HOCRFeatureExtractor,
@@ -20,31 +13,6 @@ feature_extractor_dict = {
     'PDFContractJaFeatureExtractor': feature_extractor.PDFContractJaFeatureExtractor,
     'TextContractFeatureExtractor': feature_extractor.TextContractFeatureExtractor
 }
-
-
-def dump_predictions(out_path, documents, documents_pred):
-    with open(out_path, 'w') as fout:
-        for d, d_p in zip(documents, documents_pred):
-            assert d.path == d_p.path
-            transition_prediction_accuracy = accuracy_score(
-                np.array([l.value for l in d.labels]),
-                np.array([l.value for l in d_p.labels])
-            )
-            fout.write(json.dumps({
-                'path': d.path,
-                'texts': d.texts,
-                'features': d.feature_array,
-                'transition_prediction_accuracy': transition_prediction_accuracy,
-                'ground_truth': {
-                    'labels': [l.name for l in d.labels],
-                    'pointers': d.pointers
-                },
-                'prediction': {
-                    'labels': [l.name for l in d_p.labels],
-                    'pointers': d_p.pointers
-                }
-            }))
-            fout.write('\n')
 
 
 @click.command()
@@ -72,32 +40,23 @@ def main(k_folds: int, prediction, metrics, file_type: str, feature: str, raw_di
     else:
         documents = loader.text.load_from_directory(raw_dir, annos)
 
-    print('Extracting features from documents')
     feature_extractor_cls = feature_extractor_dict[feature]
-    documents = [feature_extractor_cls.append_features_to_document(document)
-                 for document in tqdm.tqdm(documents)]
-
-    print(f'Extracted {sum(map(lambda d: d.n_blocks, documents))} lines from '
-          f'{len(documents)} documents with label distribution: '
-          f'{Counter(sum(map(lambda d: d.labels, documents), []))} for evaluation.')
-    print(f'Extracted {documents[0].n_features}-dim features and '
-          f'{documents[0].n_pointer_features}-dim pointer features.')
-    documents_pred = predictor.k_fold_train_predict(
-        documents, n_splits=k_folds)
 
     if prediction is not None:
-        dump_predictions(prediction, documents, documents_pred)
+        metrics_, prediction = evaluate(
+            documents, feature_extractor_cls, k_folds, True)
+        with open(prediction, 'w') as fout:
+            for p in prediction:
+                fout.write(json.dumps(p))
+                fout.write('\n')
+    else:
+        metrics_ = evaluate(documents, feature_extractor_cls, k_folds, False)
 
     if metrics is None:
-        print(json.dumps(evaluate_structure(documents, documents_pred), indent=2))
-        print(json.dumps(evaluate_labels(documents, documents_pred), indent=2))
+        print(json.dumps(metrics_, indent=2))
     else:
-        _metrics = {
-            'structure': evaluate_structure(documents, documents_pred),
-            'labels': evaluate_labels(documents, documents_pred)
-        }
         with open(metrics, 'w') as fout:
-            json.dump(_metrics, fout, indent=2)
+            json.dump(metrics, fout, indent=2)
 
 
 if __name__ == '__main__':
---
+++ pdf_struct/core/evaluation.py
@@ -0,0 +1,64 @@
+from collections import Counter
+from typing import List, Type
+
+import numpy as np
+import tqdm
+from sklearn.metrics import accuracy_score
+
+from pdf_struct.core import predictor
+from pdf_struct.core.document import Document
+from pdf_struct.core.feature_extractor import BaseFeatureExtractor
+from pdf_struct.core.structure_evaluation import evaluate_structure, \
+    evaluate_labels
+
+
+def _create_prediction_jsons(documents, documents_pred) -> List[dict]:
+    predictions = []
+    for d, d_p in zip(documents, documents_pred):
+        assert d.path == d_p.path
+        transition_prediction_accuracy = accuracy_score(
+            np.array([l.value for l in d.labels]),
+            np.array([l.value for l in d_p.labels])
+        )
+        predictions.append({
+            'path': d.path,
+            'texts': d.texts,
+            'features': d.feature_array,
+            'transition_prediction_accuracy': transition_prediction_accuracy,
+            'ground_truth': {
+                'labels': [l.name for l in d.labels],
+                'pointers': d.pointers
+            },
+            'prediction': {
+                'labels': [l.name for l in d_p.labels],
+                'pointers': d_p.pointers
+            }
+        })
+    return predictions
+
+
+def evaluate(documents: List[Document],
+             feature_extractor_cls: Type[BaseFeatureExtractor],
+             k_folds: int,
+             prediction: bool=False):
+    print('Extracting features from documents')
+    documents = [feature_extractor_cls.append_features_to_document(document)
+                 for document in tqdm.tqdm(documents)]
+
+    print(f'Extracted {sum(map(lambda d: d.n_blocks, documents))} lines from '
+          f'{len(documents)} documents with label distribution: '
+          f'{Counter(sum(map(lambda d: d.labels, documents), []))} for evaluation.')
+    print(f'Extracted {documents[0].n_features}-dim features and '
+          f'{documents[0].n_pointer_features}-dim pointer features.')
+    documents_pred = predictor.k_fold_train_predict(
+        documents, n_splits=k_folds)
+
+    metrics = {
+        'structure': evaluate_structure(documents, documents_pred),
+        'labels': evaluate_labels(documents, documents_pred)
+    }
+    if prediction:
+        prediction_jsons = _create_prediction_jsons(documents, documents_pred)
+        return metrics, prediction_jsons
+    else:
+        return metrics
