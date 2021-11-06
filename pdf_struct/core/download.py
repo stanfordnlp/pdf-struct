feff88090fde953ea9dbf4dc118bcb8516a95e4d
Yuta Koreeda <yuta.koreeda@hal.hitachi.com>
2021-11-05 21:41:37 -0400

    Implement file download

--- pdf_struct/core/download.py
+++ pdf_struct/core/download.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2015 Preferred Infrastructure, Inc.
 # Copyright (c) 2015 Preferred Networks, Inc.
+# Copyright (c) 2021, Hitachi America Ltd. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,22 +19,27 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 import contextlib
 import hashlib
 import os
 import shutil
 import sys
+import tempfile
 
 import filelock
 from six.moves.urllib import request
 
-
 _dataset_root = os.environ.get(
-    'CHAINER_DATASET_ROOT',
-    os.path.join(os.path.expanduser('~'), '.chainer', 'dataset'))
+    'PDFSTRUCT_DATASET_ROOT',
+    os.path.join(os.path.expanduser('~'), '.pdf-struct', 'model'))
 
 
-def get_dataset_root():
+_url_root = os.environ.get(
+    'PDFSTRUCT_URL_ROOT', 'http://localhost:8000/')
+
+
+def get_cache_root():
     """Gets the path to the root directory to download and cache datasets.
     Returns:
         str: The path to the dataset root directory.
@@ -41,58 +47,16 @@ def get_dataset_root():
     return _dataset_root
 
 
-def set_dataset_root(path):
-    """Sets the root directory to download and cache datasets.
-    There are two ways to set the dataset root directory. One is by setting the
-    environment variable ``CHAINER_DATASET_ROOT``. The other is by using this
-    function. If both are specified, one specified via this function is used.
-    The default dataset root is ``$HOME/.chainer/dataset``.
-    Args:
-        path (str): Path to the new dataset root directory.
-    """
-    global _dataset_root
-    _dataset_root = path
+def get_model_url(model_name):
+    return _url_root + model_name + '.joblib'
 
 
-def get_dataset_directory(dataset_name, create_directory=True):
-    """Gets the path to the directory of given dataset.
-    The generated path is just a concatenation of the global root directory
-    (see :func:`set_dataset_root` for how to change it) and the dataset name.
-    The dataset name can contain slashes, which are treated as path separators.
-    Args:
-        dataset_name (str): Name of the dataset.
-        create_directory (bool): If True (default), this function also creates
-            the directory at the first time. If the directory already exists,
-            then this option is ignored.
-    Returns:
-        str: Path to the dataset directory.
-    """
-    path = os.path.join(_dataset_root, dataset_name)
-    if create_directory:
-        try:
-            os.makedirs(path)
-        except OSError:
-            if not os.path.isdir(path):
-                raise
-    return path
+def get_cache_filename(url):
+    return hashlib.md5(url.encode('utf-8')).hexdigest()
 
 
 def cached_download(url):
-    """Downloads a file and caches it.
-    It downloads a file from the URL if there is no corresponding cache. After
-    the download, this function stores a cache to the directory under the
-    dataset root (see :func:`set_dataset_root`). If there is already a cache
-    for the given URL, it just returns the path to the cache without
-    downloading the same file.
-    .. note::
-        This function raises :class:`OSError` when it fails to create
-        the cache directory. In older version, it raised :class:`RuntimeError`.
-    Args:
-        url (str): URL to download from.
-    Returns:
-        str: Path to the downloaded file.
-    """
-    cache_root = os.path.join(_dataset_root, '_dl_cache')
+    cache_root = get_cache_root()
     try:
         os.makedirs(cache_root)
     except OSError:
@@ -100,14 +64,13 @@ def cached_download(url):
             raise
 
     lock_path = os.path.join(cache_root, '_dl_lock')
-    urlhash = hashlib.md5(url.encode('utf-8')).hexdigest()
-    cache_path = os.path.join(cache_root, urlhash)
+    cache_path = os.path.join(cache_root, get_cache_filename(url))
 
     with filelock.FileLock(lock_path):
         if os.path.exists(cache_path):
             return cache_path
 
-    with utils.tempdir(dir=cache_root) as temp_root:
+    with tempdir(dir=cache_root) as temp_root:
         temp_path = os.path.join(temp_root, 'dl')
         sys.stderr.write('Downloading from {}...\n'.format(url))
         sys.stderr.flush()
@@ -118,45 +81,9 @@ def cached_download(url):
     return cache_path
 
 
-def cache_or_load_file(path, creator, loader):
-    """Caches a file if it does not exist, or loads it otherwise.
-    This is a utility function used in dataset loading routines. The
-    ``creator`` creates the file to given path, and returns the content. If the
-    file already exists, the ``loader`` is called instead, and it loads the
-    file and returns the content.
-    Note that the path passed to the creator is temporary one, and not same as
-    the path given to this function. This function safely renames the file
-    created by the creator to a given path, even if this function is called
-    simultaneously by multiple threads or processes.
-    Args:
-        path (str): Path to save the cached file.
-        creator: Function to create the file and returns the content. It takes
-            a path to temporary place as the argument. Before calling the
-            creator, there is no file at the temporary path.
-        loader: Function to load the cached file and returns the content.
-    Returns:
-        It returns the returned values by the creator or the loader.
-    """
-    if os.path.exists(path):
-        return loader(path)
-
-    try:
-        os.makedirs(_dataset_root)
-    except OSError:
-        if not os.path.isdir(_dataset_root):
-            raise RuntimeError('cannot create dataset directory')
-
-    lock_path = os.path.join(_dataset_root, '_create_lock')
-
-    with utils.tempdir() as temp_dir:
-        file_name = os.path.basename(path)
-        temp_path = os.path.join(temp_dir, file_name)
-        content = creator(temp_path)
-        with filelock.FileLock(lock_path):
-            if not os.path.exists(path):
-                shutil.move(temp_path, path)
-
-    return content
+def cached_model_download(model_name):
+    url = get_model_url(model_name)
+    return cached_download(url)
 
 
 @contextlib.contextmanager
